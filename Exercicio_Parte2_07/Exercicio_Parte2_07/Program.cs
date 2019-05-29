196fc02d379f576056d6946b9a2d4692986b4905
Alberto <albertomaronezio@hotmai.com>
2019-05-29 14:50:54 -0300

    Exercicio_Parte2_07

---
+++ Exercicio_Parte2_07/Exercicio_Parte2_07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio_Parte2_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiroNumero = 0;
+            int segundoNumero = 0;
+            int terceiroNumero = 0;
+
+            Console.Write("Digite o Primeiro Número: ");
+            primeiroNumero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Segundo Número: ");
+            segundoNumero = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o Terceiro Número: ");
+            terceiroNumero = int.Parse(Console.ReadLine());
+
+            if (primeiroNumero > segundoNumero + terceiroNumero)
+            {
+                Console.WriteLine("O Primeiro número é mais do que a soma dos outros dois!");
+            }
+        }
+    }
+}
